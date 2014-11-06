9ba6e0c7dd8a9b51dcd44f1f33a04329f7d8a67e
mezuri <kipf@berkeley.edu>
2014-11-06 16:26:23 +0100

    updated scripts

--- refine.sh
+++ refine.sh
@@ -1,6 +1,12 @@
 echo 'launched refine.sh'
-/google-refine-2.5/refine -i 0.0.0.0 &
+/google-refine-2.5/refine -i 0.0.0.0 > /tmp/server-log.txt &
 echo 'called google-refine-2.5'
+
+sleep 1
+while ! grep -m1 ' Point your browser to' < /tmp/server-log.txt; do
+    sleep 1
+done
+
 export OPENREFINE_HOST=23.236.57.67
 python load_csv.py ‘/tmp/inputs/refine_input.csv’
 echo 'called load_csv.py'
